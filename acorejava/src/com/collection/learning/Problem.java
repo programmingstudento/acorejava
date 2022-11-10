02944dad8c3133e00f5f37bc8b01a627f23ca403
programmingstudento <utsabkarkee1@gmail.com>
2022-11-10 14:54:19 +0545

    Using ListIterator.

--- acorejava/src/com/collection/learning/Problem.java
+++ acorejava/src/com/collection/learning/Problem.java
@@ -2,12 +2,15 @@ package com.collection.learning;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Problem {
 	public static void main(String[] args) {
-		List<String> fruits = Arrays.asList("Apple", "Banana", "Guauva", "Peach", "Orange");
+		List<String> fruits = Arrays.asList("Apple", "Banana", "Guauva", "Peach", "Orange", "apple", "banana");
 		Collections.shuffle(fruits);
 
 		// Through for each
@@ -23,5 +26,24 @@ public class Problem {
 		System.out.println();
 		// stream
 		fruits.stream().forEach(System.out::println);
+
+		Comparator<String> one = (String first, String second) -> first.compareToIgnoreCase(second);
+		SortedSet<String> foods = new TreeSet<>(one);
+		foods.addAll(fruits);
+		System.out.println(foods);
+
+		var items = Arrays.asList("read ", " run ", " dance", " eat ", " swim");
+		for (var item : items) {
+			System.out.println(item + " " + item.length());
+		}
+
+		removeSpace(items);
+		items.forEach(item -> System.out.println(item + " " + item.length()));
+	}
+
+	public static void removeSpace(List<String> words) {
+		for (ListIterator<String> word = words.listIterator(); word.hasNext();) {
+			word.set(word.next().trim());
+		}
 	}
 }
